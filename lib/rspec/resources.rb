4853ea5981e643defb03935cff1a1fb3244613e2
David Poetzsch-Heffter <davidpoetzsch@web.de>
2018-04-13 13:49:47 +0200

    created gem via bundle

---
+++ lib/rspec/resources.rb
@@ -0,0 +1,7 @@
+require "rspec/resources/version"
+
+module Rspec
+  module Resources
+    # Your code goes here...
+  end
+end
---
+++ lib/rspec/resources/version.rb
@@ -0,0 +1,5 @@
+module Rspec
+  module Resources
+    VERSION = "0.1.0"
+  end
+end
